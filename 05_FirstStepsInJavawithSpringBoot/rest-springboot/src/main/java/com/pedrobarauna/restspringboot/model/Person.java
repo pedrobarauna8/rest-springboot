a169595f4ad675261aa9c09ace77187da51539fe
Pedro Barauna <pedro.barauna.50@outlook.com>
2023-08-22 14:56:34 -0300

    Aula 7

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-springboot/src/main/java/com/pedrobarauna/restspringboot/model/Person.java
@@ -0,0 +1,74 @@
+package com.pedrobarauna.restspringboot.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Person implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String adress;
+	private String gender;
+	
+	public Person() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(id, other.id);
+	}
+}
