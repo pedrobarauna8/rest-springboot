820ba952b3d56fdba8b76360305aa9c47dc6c2d9
Pedro Barauna <pedro.barauna.50@outlook.com>
2023-08-23 11:00:15 -0300

    Aula 12

--- 05_FirstStepsInJavawithSpringBoot/rest-springboot/src/main/java/com/pedrobarauna/restspringboot/data/vo/v1/PersonVO.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-springboot/src/main/java/com/pedrobarauna/restspringboot/data/vo/v1/PersonVO.java
@@ -3,13 +3,24 @@ package com.pedrobarauna.restspringboot.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "first_name", "last_name", "adress", "gender"})
 public class PersonVO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	
+	@JsonProperty("last_name")
 	private String lastName;
 	private String adress;
+	
+	@JsonIgnore
 	private String gender;
 	
 	public PersonVO() {
